6a194939c544bc269d864c4b27034e73c062bf8c
Ahmed Helmy <helmya777@gmail.com>
2023-07-18 18:44:19 -0700

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,21 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
  * main - print main, followed by new line
  * Return: 0
  */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
 
-int main()
+int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-    return 0;
+	return (0);
 }
